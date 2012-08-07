d3c901a12d02bdb3aed50634a6dbb82eccca466b
Magnus Runesson <M.Runesson@gmail.com>
2012-08-07 16:08:54 +0200

    Minor cleaning and some doc.

--- moff/README.txt
+++ moff/README.txt
@@ -1,1 +1,25 @@
-wget --mirror --cut-dirs=1 --random-wait -w 5 -nH -np -e robots=off  http://central.maven.org/maven2/org/apache/maven/plugins/
+Moff mirrors a Maven artifact and all its dependencies, including source and plugins.
+
+
+Usage:
+java -jar moff.jar groupId artifactId version
+
+An Maven repository is created in the moff subdirectory to the current
+directory. The directory moff is created if missed. 
+Moff will not try to download a Maven artifact if it already exist in the 
+moff-repository.
+
+
+
+
+Known issues:
+* If Moff fails to download an artifact it will crash. You may end up with
+  artifacts were all dependencies are downloaded correct.
+
+
+TODO:
+* Make tests work and create more tests.
+* Customized destination directory
+* Support for list of artifacts to download.
+
+
--- moff/src/main/scala/org/linuxalert/moff/Moff.scala
+++ moff/src/main/scala/org/linuxalert/moff/Moff.scala
@@ -15,10 +15,6 @@ object Moff {
 		
 		val newLocalFiles = downloader.download(artifact)
 		val newPoms = newLocalFiles.filter(_.endsWith(".pom")).map(new Pom(_))
-//		newPoms.map((pom:Pom) => pom.getDependencyArtifacts).flatten.foreach((a:Artifact) =>println("DepArtifacts found: " + a.getArtifactAsPath))
-//		newPoms.map((pom:Pom) => pom.getParentArtifacts).flatten.foreach((a:Artifact) =>println("ParentArtifacts found: " + a.getArtifactAsPath))
-//		newPoms.map((pom:Pom) => pom.getPluginArtifacts).flatten.foreach((a:Artifact) =>println("PluginArtifacts found: " + a.getArtifactAsPath))
-//		val newArtifacts = newPoms.map((pom:Pom) => Seq.concat(pom.getParentArtifacts(),pom.getDependencyArtifacts(),pom.getPluginArtifacts())).flatten
 		val newArtifacts = newPoms.map((pom:Pom) => Seq.concat(pom.getParentArtifacts(),pom.getDependencyArtifacts())).flatten
 		val newArtifactsWithLatest = newArtifacts.map((a:Artifact)=>{
 		  if (a.version.isEmpty()) repo.getLatestReleasedArtifact(a)
@@ -26,15 +22,19 @@ object Moff {
 		  else if (a.version.equals("LATEST")) repo.getLatestArtifact(a)
 		  else a
 		})
-//		newArtifactsWithLatest.foreach((a:Artifact)=>println("FOO: " + a.getArtifactAsPath))
 		newArtifactsWithLatest.foreach(downloadArtifact(_, downloader))
 		println
 	}
 
 
-	def main(args : Array[String]) { 
+	def main(args : Array[String]) {
+	    if (args.length!=3) {
+	        System.err.println("Wrong number of arguments.")
+	        System.err.println("Following arguments, in given order, must be: groupId artifactId version")
+	        System.exit(1)
+	    }
 		val rootArtifact = new Artifact(args(0), args(1), args(2))
-		val local = new LocalRepository("/tmp/foo")
+		val local = new LocalRepository("moff")
 		val downloader = new Downloader(local)
 		val repo = new RemoteRepository
 		rootArtifact.download(downloader, repo)
